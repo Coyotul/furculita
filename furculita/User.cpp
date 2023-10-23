72da37d73b07c50470ec82e81b7ac7c650c059db
cosminsss_alex <cosminsss.alex19@gmail.com>
2023-10-23 15:51:25 +0300

    Added getters and setters for class "User"
    I added getters and setter for the class' members.

--- furculita/User.cpp
+++ furculita/User.cpp
@@ -14,3 +14,23 @@ User::User(const User& user) :
 	m_name(user.m_name), m_score(user.m_score)
 {
 }
+
+std::string User::GetName()
+{
+	return this->m_name;
+}
+
+void User::SetName(const std::string& name)
+{
+	m_name = name;
+}
+
+uint16_t User::GetScore()
+{
+	return this->m_score;
+}
+
+void User::SetScore(uint16_t score)
+{
+	m_score = score;
+}
