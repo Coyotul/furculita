f824bd6882f4b983639bf589b28ba143d1dc0df6
Circei Andrei <andreicircei78@gmail.com>
2023-10-23 16:18:41 +0300

    added methods to game class

--- furculita/Game.cpp
+++ furculita/Game.cpp
@@ -1,3 +1,20 @@
 #include "Game.h"
 
-Game::Game() : currentPlayerIndex(0), gameInProgress(false) {}
+Game::Game() : currentPlayerIndex(0), gameInProgress(false) {
+}
+
+void Game::addPlayer(const std::string& playerName) {
+
+}
+void Game::startGame(const std::string& wordToGuess) {
+
+}
+void Game::endGame() {
+
+}
+void Game::guessWord(const std::string& guessedWord) {
+
+}
+void Game::displayScores()const {
+
+}
--- furculita/Game.h
+++ furculita/Game.h
@@ -6,12 +6,18 @@ class Game
 {
 public:
 	Game();
+	void addPlayer(const std::string& playerName);
+	void startGame(const std::string& wordToGuess);
+	void endGame();
+	void guessWord(const std::string& guessedWord);
+	void displayScores() const;
 
 private:
 	 std::string currentWord;
 	 //std::vector<Player> players;
 	 std::uint8_t currentPlayerIndex;
 	 bool gameInProgress;
+
 
 
 
