8c0046676df7c6ff7a7db59ad85e0c3c82a23992
Constantin Florin-George <constantingeorge150@gmail.com>
2023-10-27 11:38:00 +0300

    Revert "."
    This reverts commit 5f848a77d7be9a2306f687c0c860a1bba125a12e.

--- furculita/UiFileOpener.cpp
+++ furculita/UiFileOpener.cpp
@@ -1,17 +1,8 @@
 #include "UiFileOpener.h"
-#include <QtDesigner/QtDesigner>
-#include <QtDesigner/QDesignerFormWindowManagerInterface>
 
-UiFileOpener::UiFileOpener(QObject* parent) : QObject(parent) {}
+UiFileOpener::UiFileOpener(QObject *parent)
+	: QObject(parent)
+{}
 
-void UiFileOpener::openUiFile(const QString& uiFilePath) {
-    QDesignerFormWindowManagerInterface* manager = QDesignerFormWindowManagerInterface::instance();
-
-    if (manager) {
-        QDesignerFormWindowInterface* formWindow = manager->createFormWindow();
-        if (formWindow) {
-            formWindow->openUiFile(uiFilePath, true);
-            formWindow->setVisible(true);
-        }
-    }
-}
+UiFileOpener::~UiFileOpener()
+{}
