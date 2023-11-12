cda16850b0502b892bab30a1f091db8cf39ffcaa
laurcamil <laurentiu.camil@student.unitbv.ro>
2023-11-12 21:02:40 +0200

    implemented createStorage function

--- Server/furculita/WordsDb.h
+++ Server/furculita/WordsDb.h
@@ -4,6 +4,24 @@
 
 #include <sqlite_orm/sqlite_orm.h>
 namespace sql = sqlite_orm;
+
+struct Word
+{
+	int id;
+	std::pair<std::string, std::string> word;
+};
+
+inline auto createStorage(const std::string& filename)
+{
+	return sql::make_storage(
+		filename,
+		sql::make_table(
+			"Words",
+			sql::make_column("id", &Word::id, sql::primary_key().autoincrement()),
+			sql::make_column("word", &Word::word)
+		)
+	);
+}
 class WordsDb
 {
 };
