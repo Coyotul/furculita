5ee8b6aa6ca1db192e6f0b933b11231b9ea241a1
Circei Andrei <andreicircei78@gmail.com>
2023-11-15 16:34:34 +0200

    adding url params in server route

--- Server/furculita/Server.cpp
+++ Server/furculita/Server.cpp
@@ -1,4 +1,5 @@
 #include "Server.h";
+import game;
 
 
 Server::Server() {
@@ -10,9 +11,33 @@ void Server::run() {
 }
 
 void Server::configureRoutes() {
+    
 	CROW_ROUTE(app, "/")([]() {
 		
 		return "Hello Gartic";
 		});
+    CROW_ROUTE(app, "/game")
+        ([](const crow::request& req) {
+        gartic::Game game;
+        if (req.url_params.get("action") == "start") {
+            game.startGame();
+        }
+        else if (req.url_params.get("action") == "guess") {
+            std::string guessedWord = req.url_params.get("word");
+            game.guessWord(guessedWord);
+        }
+        else if (req.url_params.get("action") == "end") {
+            game.endGame();
+        }
+
+        
+        if (!game.checkGameState()) {
+            
+            return "Game over!";
+        }
+
+        
+        return "Hello Gartic 2";
+            });
 }
 
