0dfadd9c6f1527f69ecc7de21b2f9ce098e55bf0
laurcamil <laurentiu.camil@student.unitbv.ro>
2023-11-18 16:09:13 +0200

    called populate storage function in main and created database file

--- Server/furculita/furculita.cpp
+++ Server/furculita/furculita.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include "Server.h"
 import game;
+#include <sqlite_orm/sqlite_orm.h>
+namespace sql = sqlite_orm;
 
+#include "WordsDb.h"
 int main()
 {
+    Storage db = createStorage("words.sqlite");
+    db.sync_schema();
+    auto initialProductsCount = db.count<Word2>();
+    if (initialProductsCount == 0)
+    {
+        populateStorage(db);
+    }
+    
+
     gartic::Game myGame;
     myGame.startGame();
     
