bd7ab2bd9b51bd3d0d7e95028ad1ad93a19610eb
Circei Andrei <andreicircei78@gmail.com>
2023-11-20 21:18:17 +0200

    trying select random words from dataBase

--- Gartic_Server/furculita/WordsDb.cpp
+++ Gartic_Server/furculita/WordsDb.cpp
@@ -8,60 +8,94 @@
 
 void populateStorage(Storage& storage)
 {
-	std::ifstream inputFile("words.txt");
-	std::vector<WordStruct> words;
+    std::ifstream inputFile("words.txt");
+    if (!inputFile.is_open())
+    {
+        std::cerr << "Error opening file " << std::endl;
+        return;
+    }
 
-	std::string line;
-	while (std::getline(inputFile, line))
-	{
-		std::istringstream iss(line);
-		std::string word1, word2;
+    std::vector<WordStruct> words;
 
-		if (iss >> word1 >> word2)
-		{
-			WordStruct element;
-			element.id = -1;
-			element.wordInEnglish = word1;
-			element.wordInRomanian = word2;
+    std::string line;
+    while (std::getline(inputFile, line))
+    {
+        std::istringstream iss(line);
+        std::string word1, word2;
 
-			words.push_back(element);
-		}
-	}
+        if (iss >> word1 >> word2)
+        {
+            WordStruct element;
+            element.id = -1;
+            element.wordInEnglish = word1;
+            element.wordInRomanian = word2;
 
-	inputFile.close();
+            words.push_back(element);
+        }
+        else
+        {
+            std::cerr << "Error reading line: " << line << std::endl;
+        }
+    }
 
-	storage.insert_range(words.begin(), words.end());	
+    inputFile.close();
+
+    if (!words.empty())
+    {
+        storage.insert_range(words.begin(), words.end());
+        std::cout << "Database populated successfully." << std::endl;
+    }
+    else
+    {
+        std::cerr << "No data to populate the database." << std::endl;
+    }
+    
 }
 
 WordsDb::WordsDb(Storage& storage):
 	m_db { storage }
 {
-
+    
 }
 
 std::vector<WordStruct> WordsDb::getRandomWords(size_t count, const std::string& language) const
-{
-	auto allWords = m_db.get_all<WordStruct>();
+{   
+    try {
+        auto allWords = m_db.get_all<WordStruct>();
 
-	// Filter by language
-	auto languageFilter = [&language](const WordStruct& word) {
-		return (language == "english") ? !word.wordInEnglish.empty() : !word.wordInRomanian.empty();
-	};
+        auto languageFilter = [&language](const WordStruct& word) {
+            return (language == "english") ? !word.wordInEnglish.empty() : !word.wordInRomanian.empty();
+        };
+        std::cout << "Words before language filtering: ";
+        for (const auto& word : allWords) {
+            std::cout << word.wordInEnglish << " | " << word.wordInRomanian << ", ";
+        }
+        std::cout << std::endl;
+        std::cout << "Number of words before language filtering: " << allWords.size() << std::endl;
+        allWords.erase(std::remove_if(allWords.begin(), allWords.end(), languageFilter), allWords.end());
+        std::cout << "Number of words after language filtering: " << allWords.size() << std::endl;
+        // Shuffle the vector randomly
+        std::random_device rd;
+        std::mt19937 g(rd());
 
-	allWords.erase(std::remove_if(allWords.begin(), allWords.end(), languageFilter), allWords.end());
+        std::shuffle(allWords.begin(), allWords.end(), g);
 
-	// Shuffle the vector randomly
-	std::random_device rd;
-	std::mt19937 g(rd());
+        // Take the first 'count' elements
+        if (allWords.size() > count) {
+            allWords.resize(count);
+        }
 
-	std::shuffle(allWords.begin(), allWords.end(), g);
+        
 
-	// Take the first 'count' elements
-	if (allWords.size() > count) {
-		allWords.resize(count);
-	}
+        return allWords;
 
-	return allWords;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+
+    // Filter by language
+    
 }
 
 
--- Gartic_Server/furculita/furculita.cpp
+++ Gartic_Server/furculita/furculita.cpp
@@ -7,13 +7,7 @@ namespace sql = sqlite_orm;
 #include "WordsDb.h"
 int main()
 {
-    Storage db = createStorage("words.sqlite");
-    db.sync_schema();
-    auto initialProductsCount = db.count<WordStruct>();
-    if (initialProductsCount == 0)
-    {
-        populateStorage(db);
-    }
+    
     
 
     gartic::Game myGame;
