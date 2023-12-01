7497c839a81173775db76e626be63e3454dbbd68
Circei Andrei <andreicircei78@gmail.com>
2023-12-01 17:30:43 +0200

    connecting addPlayer from client to server

--- Gartic_Server/furculita/Server.cpp
+++ Gartic_Server/furculita/Server.cpp
@@ -11,43 +11,50 @@ void Server::run() {
 }
 
 void Server::configureRoutes() {
-    gartic::Game game;
+	gartic::Game game;
 	CROW_ROUTE(app, "/")([]() {
-		
+
 		return "Hello Gartic";
+
 		});
-    CROW_ROUTE(app, "/game")
-        ([&game](const crow::request& req) -> crow::response {
-        
-        if (req.url_params.get("action") == "start") {
-            game.startGame();
-        }
-        else if (req.url_params.get("action") == "guess") {
-            std::string guessedWord = req.url_params.get("word");
-            game.guessWord(guessedWord);
-        }
-        else if (req.url_params.get("action") == "end") {
-            game.endGame();
-        }
-        else if (req.url_params.get("action") == "addPlayer") {
-            std::string playerName = req.url_params.get("playerName");
-            game.addPlayer(playerName);
-            return crow::response{ "Player added: " + playerName };
-        }
-        else if (req.url_params.get("action") == "reset") {
-            game.resetGame();
-            return crow::response{ "Game reset." };
-        }
-        
 
-        
-        if (!game.checkGameState()) {
-            
-            return crow::response{ "Game over!" };
-        }
+	CROW_ROUTE(app, "/addPlayer")([&game](const crow::request& req)->crow::response {
+		if (req.method == crow::HTTPMethod::Post) {
 
-        
-        return crow::response{ "Hello Gartic 2" };
-            });
+			std::string playerName = req.url_params.get("playerName");
+
+		}
+
+
+		});
+
+	CROW_ROUTE(app, "/game")
+		([&game](const crow::request& req) -> crow::response {
+
+		if (req.url_params.get("action") == "start") {
+			game.startGame();
+		}
+		else if (req.url_params.get("action") == "guess") {
+			std::string guessedWord = req.url_params.get("word");
+			game.guessWord(guessedWord);
+		}
+		else if (req.url_params.get("action") == "end") {
+			game.endGame();
+		}
+		else if (req.url_params.get("action") == "reset") {
+			game.resetGame();
+			return crow::response{ "Game reset." };
+		}
+
+
+
+		if (!game.checkGameState()) {
+
+			return crow::response{ "Game over!" };
+		}
+
+
+		return crow::response{ "Hello Gartic 2" };
+			});
 }
 
