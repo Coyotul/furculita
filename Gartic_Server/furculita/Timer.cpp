8db63e48b5733ff21e4f9b40ace7e9da8bd5f811
laurcamil <laurentiu.camil@student.unitbv.ro>
2023-12-03 17:48:03 +0200

    created function definitions in timer.cpp

---
+++ Gartic_Server/furculita/Timer.cpp
@@ -0,0 +1,19 @@
+#include "Timer.h"
+
+Timer* Timer::Instance()
+{
+    return nullptr;
+}
+
+void Timer::Release()
+{
+}
+
+void Timer::Reset()
+{
+}
+
+float Timer::GetDeltaTime()
+{
+    return 0.0f;
+}
