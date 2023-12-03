bc70281f54ed4763b12a4f581acaf2031cf99662
laurcamil <laurentiu.camil@student.unitbv.ro>
2023-12-03 17:45:20 +0200

    added public functions

---
+++ Gartic_Server/furculita/Timer.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <chrono>
+class Timer
+{
+private:
+
+	static Timer* sInstance;
+	std::chrono::system_clock::time_point m_StartTime;
+	std::chrono::duration<float> m_DeltaTime;
+	float m_TimeScale;
+
+public:
+	static Timer* Instance();
+	static void Release();
+
+	void Reset();
+	float GetDeltaTime();
+	void SetTimeScale(float t = 1.0f);
+	float GetTimeScale();
+	void Tick();
+};
+
