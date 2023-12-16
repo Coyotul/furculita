46dcbbdfc0a717d34b252581df3079b52b27ccbd
cosminsss_alex <cosminsss.alex19@gmail.com>
2023-12-16 23:49:00 +0200

    Timer private members

--- Gartic_Server/Timer/Timer.h
+++ Gartic_Server/Timer/Timer.h
@@ -9,5 +9,11 @@
 
 class TIMER_API Timer
 {
-
+private:
+	static Timer* sInstance;
+	std::chrono::system_clock::time_point m_StartTime;
+	std::chrono::duration<float> m_DeltaTime;
+	float m_TimeScale;
+	Timer();
+	~Timer();
 };
