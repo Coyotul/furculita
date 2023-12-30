626d048791a1c56c6fdc4984d78b58a24726a8a8
laurcamil <laurentiu.camil@student.unitbv.ro>
2023-12-30 16:31:45 +0200

    implemented log function

--- Gartic_Server/furculita/Logger.h
+++ Gartic_Server/furculita/Logger.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <chrono>
 class Logger
 {
 public:
@@ -9,5 +10,37 @@ public:
 		Warning,
 		Error
 	};
+public:
+	template <typename... Args>
+	static void Log(Level level, Args&&... args)
+	{
+		switch (level)
+		{
+			using enum Level;
+		case Info:
+			std::cout << "[Info]";
+			break;
+		case Warning:
+			std::cout << "[Warning]";
+			break;
+		case Error:
+			std::cout << "[Error]";
+			break;
+
+		}
+
+		using std::chrono::system_clock;
+		using std::chrono::sys_seconds;
+		using std::chrono::floor;
+		using std::chrono::seconds;
+
+		sys_seconds now{ floor<seconds>(system_clock::now()) };
+		std::cout << std::format("[{:%Y-%m-%d %H:%M:%S}]", now);
+
+		((std::cout << ' ' << std::forward<Args>(args)), ...);
+
+		std::cout << '\n';
+	}
 };
+
 
