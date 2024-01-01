83ca93404ceb726b149030519415dbd9047fa45a
Circei Andrei <andreicircei78@gmail.com>
2024-01-01 17:10:57 +0200

    trying to solve the drawing

--- Gartic_Client/Client/Client/Gartic.cpp
+++ Gartic_Client/Client/Client/Gartic.cpp
@@ -9,19 +9,37 @@
 #include <crow.h>
 #include <fstream>
 
+#include <QGraphicsPixmapItem>
+#include <QPainter>
+#include <QPaintEvent>
+
+
+#include <QList>
+#include <QMainWindow>
+#include <QtWidgets>
 
 std::ofstream f("output.out");
 
 // At Start
-Gartic::Gartic(QWidget *parent)
+Gartic::Gartic(QWidget* parent)
     : QMainWindow(parent)
 {
+    
     ui.setupUi(this);
 
+    
     scene = new QGraphicsScene(this);
-    drawRectItem = new QGraphicsRectItem(0, 0, 600, 600); 
-    scene->addItem(drawRectItem);
-    ui.drawView->setScene(scene); 
+
+    
+    scribbleArea = new ScribbleArea;
+
+    
+    scene->addWidget(scribbleArea);
+
+    
+    ui.drawView->setScene(scene);
+
+    
     currentDrawing = nullptr;
     hideInterface();
     username = "Guest";
@@ -29,64 +47,492 @@ Gartic::Gartic(QWidget *parent)
     updatePlayers();
     updateLeaderboard();
     ui.easterEgg->hide();
+    ui.drawView->setMouseTracking(true);
+
+    
+    createActions();
+    createMenus();
+
+   
+    setWindowTitle(tr("Scribble Application"));
+
+    
 }
-bool Gartic::eventFilter(QObject* obj, QEvent* event)
+
+//START
+//START
+//START
+//START
+
+//void Gartic::closeEvent(QCloseEvent* event)
+//
+//{
+//
+//    // If they try to close maybeSave() returns true
+//
+//    // if no changes have been made and the app closes
+//
+//    if (maybeSave()) {
+//
+//        event->accept();
+//
+//    }
+//    else {
+//
+//
+//
+//        // If there have been changes ignore the event
+//
+//        event->ignore();
+//
+//    }
+//
+//}
+
+
+
+// Check if the current image has been changed and then
+
+// open a dialog to open a file
+
+//void Gartic::open()
+//
+//{
+//
+//    // Check if changes have been made since last save
+//
+//    // maybeSave() returns true if no changes have been made
+//
+//    if (maybeSave()) {
+//
+//
+//
+//        // Get the file to open from a dialog
+//
+//        // tr sets the window title to Open File
+//
+//        // QDir opens the current dirctory
+//
+//        QString fileName = QFileDialog::getOpenFileName(this,
+//
+//            tr("Open File"), QDir::currentPath());
+//
+//
+//
+//        // If we have a file name load the image and place
+//
+//        // it in the scribbleArea
+//
+//        if (!fileName.isEmpty())
+//
+//            scribbleArea->openImage(fileName);
+//
+//    }
+//
+//}
+
+
+
+// Called when the user clicks Save As in the menu
+
+//void Gartic::save()
+//
+//{
+//
+//    // A QAction represents the action of the user clicking
+//
+//    QAction* action = qobject_cast<QAction*>(sender());
+//
+//
+//
+//    // Stores the array of bytes of the users data
+//
+//    QByteArray fileFormat = action->data().toByteArray();
+//
+//
+//
+//    // Pass it to be saved
+//
+//    saveFile(fileFormat);
+//
+//}
+
+
+
+// Opens a dialog to change the pen color
+
+void Gartic::penColor()
+
 {
-    if (obj == ui.drawView->viewport())
-    {
-        if (event->type() == QEvent::MouseButtonPress)
-        {
-            QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
-            if (mouseEvent->button() == Qt::LeftButton)
-            {
-                ui.drawView->setFocus();  // Setează focusul pe widget-ul de desen
 
-                QPainterPath path;
-                path.moveTo(ui.drawView->mapToScene(mouseEvent->pos()));
+    // Store the chosen color from the dialog
 
-                QGraphicsPathItem* drawnPath = new QGraphicsPathItem(path);
-                scene->addItem(drawnPath);
+    QColor newColor = QColorDialog::getColor(scribbleArea->penColor());
 
-                currentDrawing = drawnPath;
 
-                return true;  // Consumă evenimentul
-            }
-        }
-        else if (event->type() == QEvent::MouseButtonRelease)
-        {
-            QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
-            if (mouseEvent->button() == Qt::LeftButton)
-            {
-                currentDrawing = nullptr;
 
-                return true;  // Consumă evenimentul
-            }
-        }
-        else if (event->type() == QEvent::MouseMove)
-        {
-            if (currentDrawing)
-            {
-                QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
-                if (ui.drawView->rect().contains(mouseEvent->pos()))
-                {
-                    QPainterPath path = currentDrawing->path();
-                    path.lineTo(ui.drawView->mapToScene(mouseEvent->pos()));
-                    currentDrawing->setPath(path);
-                }
-                else
-                {
-                    currentDrawing = nullptr;
-                }
+    // If a valid color set it
 
-                return true;  // Consumă evenimentul
-            }
-        }
-    }
+    if (newColor.isValid())
 
-    return QMainWindow::eventFilter(obj, event);
+        scribbleArea->setPenColor(newColor);
+
 }
 
 
+
+// Opens a dialog that allows the user to change the pen width
+
+void Gartic::penWidth()
+
+{
+
+    // Stores button value
+
+    bool ok;
+
+
+
+    // tr("Scribble") is the title
+
+    // the next tr is the text to display
+
+    // Get the current pen width
+
+    // Define the min, max, step and ok button
+
+    int newWidth = QInputDialog::getInt(this, tr("Scribble"),
+
+        tr("Select pen width:"),
+
+        scribbleArea->penWidth(),
+
+        1, 50, 1, &ok);
+
+    // Change the pen width
+
+    if (ok)
+
+        scribbleArea->setPenWidth(newWidth);
+
+}
+
+
+
+// Open an about dialog
+
+//void Gartic::about()
+//
+//{
+//
+//    // Window title and text to display
+//
+//    QMessageBox::about(this, tr("About Scribble"),
+//
+//        tr("<p>The <b>Scribble</b> example is awesome</p>"));
+//
+//}
+
+
+
+// Define menu actions that call functions
+
+void Gartic::createActions()
+
+{
+
+    // Create the action tied to the menu
+
+   /* openAct = new QAction(tr("&Open..."), this);*/
+
+
+
+    // Define the associated shortcut key
+
+   /* openAct->setShortcuts(QKeySequence::Open);*/
+
+
+
+    // Tie the action to MainWindow::open()
+
+   /* connect(openAct, SIGNAL(triggered()), this, SLOT(open()));*/
+
+
+
+    // Get a list of the supported file formats
+
+    // QImageWriter is used to write images to files
+    
+    ////aici incepe comentariul
+    //foreach(QByteArray format, QImageWriter::supportedImageFormats()) {
+
+    //    QString text = tr("%1...").arg(QString(format).toUpper());
+
+
+
+    //    // Create an action for each file format
+
+    //    QAction* action = new QAction(text, this);
+
+
+
+    //    // Set an action for each file format
+
+    //    action->setData(format);
+
+
+
+    //    // When clicked call MainWindow::save()
+
+    //    connect(action, SIGNAL(triggered()), this, SLOT(save()));
+
+
+
+    //    // Attach each file format option menu item to Save As
+
+    //    saveAsActs.append(action);
+
+    //}
+
+
+
+    //// Create print action and tie to MainWindow::print()
+
+    //printAct = new QAction(tr("&Print..."), this);
+
+    //connect(printAct, SIGNAL(triggered()), scribbleArea, SLOT(print()));
+
+
+
+    //// Create exit action and tie to MainWindow::close()
+
+    //exitAct = new QAction(tr("E&xit"), this);
+
+    //exitAct->setShortcuts(QKeySequence::Quit);
+
+    //connect(exitAct, SIGNAL(triggered()), this, SLOT(close()));
+
+
+
+    // Create pen color action and tie to MainWindow::penColor()
+
+    penColorAct = new QAction(tr("&Pen Color..."), this);
+
+    connect(penColorAct, SIGNAL(triggered()), this, SLOT(penColor()));
+
+
+
+    // Create pen width action and tie to MainWindow::penWidth()
+
+    penWidthAct = new QAction(tr("Pen &Width..."), this);
+
+    connect(penWidthAct, SIGNAL(triggered()), this, SLOT(penWidth()));
+
+
+
+    // Create clear screen action and tie to MainWindow::clearImage()
+
+    clearScreenAct = new QAction(tr("&Clear Screen"), this);
+
+    clearScreenAct->setShortcut(tr("Ctrl+L"));
+
+    connect(clearScreenAct, SIGNAL(triggered()),
+
+        scribbleArea, SLOT(clearImage()));
+
+
+
+    // Create about action and tie to MainWindow::about()
+
+    //aboutAct = new QAction(tr("&About"), this);
+
+    //connect(aboutAct, SIGNAL(triggered()), this, SLOT(about()));
+
+
+
+    //// Create about Qt action and tie to MainWindow::aboutQt()
+
+    //aboutQtAct = new QAction(tr("About &Qt"), this);
+
+    //connect(aboutQtAct, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
+
+}
+
+
+
+// Create the menubar
+
+void Gartic::createMenus()
+
+{
+
+    // Create Save As option and the list of file types
+
+    //saveAsMenu = new QMenu(tr("&Save As"), this);
+
+    //foreach(QAction * action, saveAsActs)
+
+    //    saveAsMenu->addAction(action);
+
+
+
+    //// Attach all actions to File
+
+    //fileMenu = new QMenu(tr("&File"), this);
+
+    //fileMenu->addAction(openAct);
+
+    //fileMenu->addMenu(saveAsMenu);
+
+    //fileMenu->addAction(printAct);
+
+    //fileMenu->addSeparator();
+
+    //fileMenu->addAction(exitAct);
+
+
+
+    // Attach all actions to Options
+
+    optionMenu = new QMenu(tr("&Options"), this);
+
+    optionMenu->addAction(penColorAct);
+
+    optionMenu->addAction(penWidthAct);
+
+    optionMenu->addSeparator();
+
+    optionMenu->addAction(clearScreenAct);
+
+
+
+    // Attach all actions to Help
+
+    /*helpMenu = new QMenu(tr("&Help"), this);
+
+    helpMenu->addAction(aboutAct);
+
+    helpMenu->addAction(aboutQtAct);*/
+
+
+
+    // Add menu items to the menubar
+
+    //menuBar()->addMenu(fileMenu);
+
+    menuBar()->addMenu(optionMenu);
+
+    //menuBar()->addMenu(helpMenu);
+
+}
+
+
+
+//bool Gartic::maybeSave()
+//
+//{
+//
+//    // Check for changes since last save
+//
+//    if (scribbleArea->isModified()) {
+//
+//        QMessageBox::StandardButton ret;
+//
+//
+//
+//        // Scribble is the title
+//
+//        // Add text and the buttons
+//
+//        ret = QMessageBox::warning(this, tr("Scribble"),
+//
+//            tr("The image has been modified.\n"
+//
+//                "Do you want to save your changes?"),
+//
+//            QMessageBox::Save | QMessageBox::Discard
+//
+//            | QMessageBox::Cancel);
+//
+//
+//
+//        // If save button clicked call for file to be saved
+//
+//        if (ret == QMessageBox::Save) {
+//
+//            return saveFile("png");
+//
+//
+//
+//            // If cancel do nothing
+//
+//        }
+//        else if (ret == QMessageBox::Cancel) {
+//
+//            return false;
+//
+//        }
+//
+//    }
+//
+//    return true;
+//
+//}
+
+
+
+//bool Gartic::saveFile(const QByteArray& fileFormat)
+//
+//{
+//
+//    // Define path, name and default file type
+//
+//    QString initialPath = QDir::currentPath() + "/untitled." + fileFormat;
+//
+//
+//
+//    // Get selected file from dialog
+//
+//    // Add the proper file formats and extensions
+//
+//    QString fileName = QFileDialog::getSaveFileName(this, tr("Save As"),
+//
+//        initialPath,
+//
+//        tr("%1 Files (*.%2);;All Files (*)")
+//
+//        .arg(QString::fromLatin1(fileFormat.toUpper()))
+//
+//        .arg(QString::fromLatin1(fileFormat)));
+//
+//
+//
+//    // If no file do nothing
+//
+//    if (fileName.isEmpty()) {
+//
+//        return false;
+//
+//    }
+//    else {
+//
+//
+//
+//        // Call for the file to be saved
+//
+//        return scribbleArea->saveImage(fileName, fileFormat.constData());
+//
+//    }
+//
+//}
+
+
+
+
+//FINISH
+//FINISH
+//FINISH
+//FINISH
 Gartic::~Gartic()
 {}
 
@@ -98,7 +544,7 @@ void Gartic::on_wordButton_1_clicked()
         word = ui.wordButton_1->text();
         sendWordToServer(word);
         qDebug() << "Word selected: " << word;
-        ui.wordText->setText(QString::fromStdString("Draw: ") + word);
+        ui.wordText->setText("Draw: " + word);
         hideWordChoices();
     }
 }
@@ -111,7 +557,7 @@ void Gartic::on_wordButton_2_clicked()
         word = ui.wordButton_2->text();
         sendWordToServer(word);
         qDebug() << "Word selected: " << word;
-        ui.wordText->setText(QString::fromStdString("Draw: ") + word);
+        ui.wordText->setText("Draw: " + word);
         hideWordChoices();
     }
 }
@@ -125,9 +571,9 @@ void Gartic::on_wordButton_3_clicked()
         sendWordToServer(word);
         qDebug() << "Word selected: " << word;
         if(language ==2)
-        ui.wordText->setText(QString::fromStdString("Draw: ") + word);
+        ui.wordText->setText("Draw: " + word);
         else
-            ui.wordText->setText(QString::fromStdString("Desen: ") + word);
+            ui.wordText->setText("Desen: " + word);
         hideWordChoices();
     }
 }
@@ -205,50 +651,7 @@ void Gartic::keyPressEvent(QKeyEvent* event)
 }
 
 
-void Gartic::mouseReleaseEvent(QMouseEvent* event)
-{
-    if (event->button() == Qt::LeftButton)
-    {
-        isDrawing = false;
-        lastMousePos = event->pos();
-    }
-}
 
-void Gartic::mousePressEvent(QMouseEvent* event)
-{
-    if (event->button() == Qt::LeftButton && ui.drawView->rect().contains(event->pos()))
-    {
-        std::cout << event->pos().x() << " " << event->pos().y() << '\n';
-        // Adaugă un nou PainterPath la vectorul de linii
-        QPainterPath newPath;
-        lastMousePos = ui.drawView->mapToScene(event->pos());
-        newPath.moveTo(lastMousePos);
-        lines.push_back(newPath);
-
-        isDrawing = true;
-    }
-}
-
-void Gartic::paintEvent(QPaintEvent* event)
-{
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing);
-
-    for (const auto& path : lines)
-    {
-        painter.drawPath(path);
-    }
-
-    // Desenează linia curentă dacă se află în proces de desenare
-    if (isDrawing)
-    {
-        QPainterPath currentPath;
-        currentPath.moveTo(lastMousePos);
-        currentPath.lineTo(ui.drawView->mapToScene(ui.drawView->mapFromGlobal(QCursor::pos())));
-        painter.drawPath(currentPath);
-        sendDrawingToServer();
-    }
-}
 
 void Gartic::sendDrawingToServer()
 {
